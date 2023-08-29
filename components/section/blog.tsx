c4b6b17232120c830d48aee8811da96523a5586e
julesdo <julescamilledore@gmail.com>
2023-08-29 15:17:58 +0200

    maj 1.11

--- components/section/blog.tsx
+++ components/section/blog.tsx
@@ -18,7 +18,7 @@ export default async function BlogSection() {
                 </div>
                 <div className="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-3">
                     {posts.map((post) => (
-                        <article key={post.id} className="flex flex-col transition-all p-4 bg-indigo-950 rounded-lg bg-background hover:border border-primary/80 hover:border-primary/100 transition-color shadow-[10px_10px_60px_10px] shadow-primary/0 hover:shadow-primary/60 items-start justify-between">
+                        <article key={post.id} className="flex flex-col transition-all p-4 bg-indigo-950 rounded-lg hover:border border-primary/80 hover:border-primary/100 transition-color shadow-[10px_10px_60px_10px] shadow-primary/0 hover:shadow-primary/60 items-start justify-between">
                             <div className="relative w-full">
                                 <img
                                     src={post.image[0].url}
--- components/section/preview.tsx
+++ components/section/preview.tsx
@@ -4,7 +4,7 @@ import { buttonVariants } from "../ui/button";
 export default function PreviewSection() {
 
     return (
-        <section className="bg-background justify-center text-left grid items-center gap-6 pb-8 pt-6 md:py-24 md:pb-0 bg-indigo-950">
+        <section className="justify-center text-left grid items-center gap-6 pb-8 pt-6 md:py-24 md:pb-0 bg-indigo-950">
             <div className="flex flex-col items-center justify-center">
                 <img alt="" src="/preview.png" className="md:w-[70vw] md:py-0 py-10" />
             </div>
