3d4c318fee5b0508087f7e0833d6e3eef482c0bb
julesdo <julescamilledore@gmail.com>
2023-09-04 10:42:31 +0200

    maj 1.16

--- components/section/pricing.tsx
+++ components/section/pricing.tsx
@@ -5,46 +5,62 @@ import { CheckIcon } from 'lucide-react'
 import { Button } from '../ui/button'
 import Link from 'next/link'
 
-const frequencies = [
+export const frequencies = [
     { value: 'monthly', label: 'Monthly', priceSuffix: '/month' },
     { value: 'annually', label: 'Annually', priceSuffix: '/year' },
 ]
-const tiers = [
+
+export const plans = [
+    {
+        name: 'Free',
+        id: 'free',
+        description: 'Essentials to get your business started.',
+        price: { monthly: 0, annually: 0 },
+        products: 2,
+        posts: 5,
+        basicSupport: true,
+        active: true,
+    },
+    {
+        name: 'Individual',
+        id: 'Individual',
+        description: 'Essentials to get your business started.',
+        price: { monthly: 9, annually: 85 },
+        products: 5,
+        posts: 10,
+        basicSupport: true,
+    },
     {
         name: 'Freelancer',
         id: 'freelancer',
-        href: 'https://app.supercontent.dev',
         description: 'Essentials for your work.',
-        price: { monthly: '$15', annually: '$144' },
-        features: ['5 products', '10 posts', 'Stripe integration', '48-hour support'],
-        mostPopular: true,
-        available: true,
-        freeTrial: true,
+        price: { monthly: 19, annually: 182 },
+        products: 10,
+        posts: 20,
+        basicSupport: true,
+        active: true,
     },
     {
         name: 'Startup',
         id: 'startup',
-        href: '#',
         description: 'Scales with your business.',
-        price: { monthly: '$30', annually: '$288' },
-        features: ['25 products', '50 posts', 'Stripe integration', '24-hour support', 'Marketing automations'],
-        mostPopular: false,
-        available: false,
-        freeTrial: true,
+        price: { monthly: 49, annually: 470 },
+        products: 50,
+        posts: 100,
+        basicSupport: true,
+        active: true,
     },
     {
         name: 'Enterprise',
         id: 'enterprise',
-        href: '#',
         description: 'Dedicated support and infrastructure with AI capabilities and Stripe integration.',
-        price: { monthly: '$48', annually: '$576' },
-        features: ['Unlimited products', 'Unlimited posts', 'Stripe integration', '1-hour dedicated support', 'Marketing automations', 'AI-powered insights'],
-        mostPopular: false,
-        available: false,
-        freeTrial: true,
+        price: { monthly: 89, annually: 854 },
+        products: 'Unlimited',
+        posts: 'Unlimited',
+        basicSupport: true,
+        active: true,
     },
 ];
-
 
 function classNames(...classes: string[]) {
     return classes.filter(Boolean).join(' ')
@@ -81,46 +97,55 @@ export default function Example() {
                     </div>
                 </div>
                 <div className="isolate mx-auto mt-10 grid max-w-md grid-cols-1 gap-8 lg:mx-0 lg:max-w-none lg:grid-cols-3">
-                    {tiers.map((tier) => (
+                    {plans.map((plan) => (
                         <div
-                            key={tier.id}
+                            key={plan.id}
                             className={classNames(
-                                tier.mostPopular ? 'bg-white/5 ring-2 ring-primary' : 'ring-1 ring-white/10',
+                                plan.id === 'freelancer' ? 'bg-white/5 ring-2 ring-primary' : 'ring-1 ring-white/10',
                                 'rounded-3xl p-8 xl:p-10'
                             )}
                         >
                             <div className="flex items-center justify-between gap-x-4">
-                                <h3 id={tier.id} className="text-lg font-semibold leading-8 text-white">
-                                    {tier.name}
+                                <h3 id={plan.id} className="text-lg font-semibold leading-8 text-white">
+                                    {plan.name}
                                 </h3>
-                                {tier.mostPopular ? (
+                                {plan.id === 'freelancer' ? (
                                     <p className="rounded-full bg-primary px-2.5 py-1 text-xs font-semibold leading-5 text-white">
                                         Most popular
                                     </p>
                                 ) : null}
                             </div>
-                            <p className="mt-4 text-sm leading-6 text-gray-300">{tier.description}</p>
+                            <p className="mt-4 text-sm leading-6 text-gray-300">{plan.description}</p>
                             <p className="mt-6 flex items-baseline gap-x-1">
-                                {// @ts-ignore
-                                 <span className="text-4xl font-bold tracking-tight text-white">{tier.price[frequency.value]}</span>}
+                                {frequency.value === 'monthly' ? (
+                                    <span className="text-4xl font-bold tracking-tight text-white">${plan.price.monthly}</span>
+                                ) : (
+                                    <span className="text-4xl font-bold tracking-tight text-white">${plan.price.annually}</span>
+                                )}
                                 <span className="text-sm font-semibold leading-6 text-gray-300">{frequency.priceSuffix}</span>
                             </p>
                             <Link
-                                href={tier.href}
-                                aria-describedby={tier.id}
-                                data-umami-event-pricingChoice={tier.name}
+                                href={plan.id === 'freelancer' ? 'https://app.supercontent.dev' : '#'}
+                                aria-describedby={plan.id}
+                                data-umami-event-pricingChoice={plan.name}
                                 className={classNames(
-                                    tier.mostPopular
+                                    plan.id === 'freelancer'
                                         ? 'bg-primary text-white shadow-sm hover:bg-indigo-400 focus-visible:outline-primary'
                                         : 'bg-white/10 text-white hover:bg-white/20 focus-visible:outline-white',
                                     'mt-6 block rounded-md py-2 px-3 text-center text-sm font-semibold leading-6 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2'
                                 )}
                             >
-                                {tier.available ? tier.freeTrial ? "Try 14-day free trial" : "Get started" : 'Coming soon'}
+                                {plan.id === 'freelancer' ? (plan.active  ? 'Try 14-day free trial' : 'Get started') : 'Coming soon'}
                             </Link>
                             <ul role="list" className="mt-8 space-y-3 text-sm leading-6 text-gray-300 xl:mt-10">
-                                {tier.features.map((feature) => (
-                                    <li key={feature} className="flex gap-x-3">
+                                {[
+                                    `${plan.products} products`,
+                                    `${plan.posts} posts`,
+                                    'Stripe integration',
+                                    plan.basicSupport ? 'Basic support' : '',
+                                    'Marketing automations',
+                                ].map((feature, index) => (
+                                    <li key={index} className="flex gap-x-3">
                                         <CheckIcon className="h-6 w-5 flex-none text-white" aria-hidden="true" />
                                         {feature}
                                     </li>
