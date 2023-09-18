edc7a6d6d8e879bc0d3dd95d8b587e16e70b2bd0
julesdo <julescamilledore@gmail.com>
2023-09-18 12:38:04 +0200

    maj 1.39

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 git add .
-git commit -m "maj 1.38"
+git commit -m "maj 1.39"
 git push -u origin main 
--- components/section/preview.tsx
+++ components/section/preview.tsx
@@ -1,12 +1,13 @@
 import Link from "next/link";
 import { buttonVariants } from "../ui/button";
+import Image from "next/image";
 
 export default function PreviewSection() {
 
     return (
         <section id="preview" className="justify-center text-left grid items-center gap-6 pb-8 pt-6 md:py-24 md:pb-0 bg-indigo-950">
             <div className="flex flex-col items-center justify-center">
-                <img alt="" src="/preview.png" className="md:w-[70vw] md:py-0 py-10" />
+                <Image alt="" src="/preview.png" className="md:py-0 py-10" width={1000} height={600} sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"/>
             </div>
         </section>
     )
