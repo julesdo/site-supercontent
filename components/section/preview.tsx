342da893c714de784633fbbc5a48c21507c66817
julesdo <julescamilledore@gmail.com>
2023-09-22 13:29:23 +0200

    maj 1.43

--- components/section/preview.tsx
+++ components/section/preview.tsx
@@ -7,7 +7,7 @@ export default function PreviewSection() {
     return (
         <section id="preview" className="justify-center text-left grid items-center gap-6 pb-8 pt-6 md:py-0 md:pb-0 bg-indigo-950">
             <div className="flex flex-col items-center justify-center px-10 py-0 md:px-0 md:py-10">
-                <Image alt="" src="/preview.gif" className="md:py-0 py-10 rounded-xl" width={1000} height={600} sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"/>
+                <video src="/preview.webm" autoPlay loop muted playsInline className="md:py-0 py-10 rounded-xl" width={1000} height={600}/>
             </div>
         </section>
     )
